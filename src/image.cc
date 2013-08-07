1b0ef3c3bfdff5f7258e544d33b54db09767c02a
Liam <github@networkimprov.net>
2013-08-06 22:12:20 -0700

    todo: image.cc loops around checkArguments

--- src/image.cc
+++ src/image.cc
@@ -13,12 +13,10 @@ void Image::Init(Handle<Object> target) {
   target->Set(String::NewSymbol("Image"), constructor_template->GetFunction());
 }
 
+//fix: comment describing these symbols
 enum {
-  eNew2,
-  eNew3,
-  eWrite1,
-  eWrite2,
-  eWrite3,
+  eNew2, eNew3,//fix: new1 ?
+  eWrite1, eWrite2, eWrite3,
   eWriteFile
 };
 
@@ -26,27 +24,29 @@ enum {
 static int kNew1[] = { eEnd };
 static int kNew2[] = { eObjectGeometry, eObjectColor, eEnd };
 static int kNew3[] = { eString, -eFunction, eEnd };
-static int* kNewSet[] = { kNew1, kNew3 };
+static int* kNewSet[] = { kNew1, kNew3 }; //fix: not used?; omits new2
 Handle<Value> Image::New(const Arguments& args) {
   HandleScope scope;
   int aOpt[1];
   Image* that = new Image();
   that->Wrap(args.This());
   try {
-    if (checkArguments(kNew1, args, aOpt))
+    if (checkArguments(kNew1, args, aOpt)) //fix: if there's a lot of these, write this as a loop
       that->setImage(new Magick::Image());
     else if (checkArguments(kNew2, args, aOpt))
       generic_start<Image>(eNew2, args, kNew2, false, aOpt);
     else if (checkArguments(kNew3, args, aOpt))
       generic_start<Image>(eNew3, args, kNew3, false, aOpt);
   } catch (std::exception& err) {
-    return ThrowException(v8::Exception::Error(String::New(err.what())));
+    return ThrowException(Exception::Error(String::New(err.what())));
   }
   return args.This();
 }
 
 static int kWriteFile[] = { eString, -eFunction, eEnd };
-Handle<Value> Image::WriteFile(const Arguments& args) { return generic_start<Image>(eWriteFile, args, kWriteFile); }
+Handle<Value> Image::WriteFile(const Arguments& args) {
+  return generic_start<Image>(eWriteFile, args, kWriteFile);
+}
 
 static int kWrite1[] = { eString, -eFunction, eEnd };
 static int kWrite2[] = { -eFunction, eEnd };
@@ -55,7 +55,7 @@ static int* kWriteSet[] = { kWrite1, kWrite2, kWrite3 };
 Handle<Value> Image::Write(const Arguments& args) {
   HandleScope scope;
   int aOpt[1];
-  if (checkArguments(kWrite1, args, aOpt))
+  if (checkArguments(kWrite1, args, aOpt)) //fix: loop?
     return generic_start<Image>(eWrite1, args, kWrite1, false, aOpt);
   else if (checkArguments(kWrite2, args, aOpt))
     return generic_start<Image>(eWrite2, args, kWrite2, false, aOpt);
@@ -73,8 +73,14 @@ void Image::Generic_process(void* pData, void* pThat) {
     that->setImage(new Magick::Image(*((Magick::Geometry*) data->val[0].pointer), *((Magick::Color*) data->val[1].pointer)));
     data->retVal.pointer = that;
     break;
-  case eNew3: that->setImage(new Magick::Image(*data->val[0].string)); data->retVal.pointer = that; break;
-  case eWriteFile: that->getImage().write(*data->val[0].string); data->retVal.pointer = that;       break;
+  case eNew3:
+    that->setImage(new Magick::Image(*data->val[0].string));
+    data->retVal.pointer = that;
+    break;
+  case eWriteFile:
+    that->getImage().write(*data->val[0].string);
+    data->retVal.pointer = that;
+    break;
   case eWrite1:
   case eWrite2:
   case eWrite3: {
@@ -87,7 +93,8 @@ void Image::Generic_process(void* pData, void* pThat) {
     //TODO delete aBlob on exception
     data->retVal.pointer = aBlob;
   } break;
-  default: assert(0);
+  default:
+    assert(0);
   }
 }
 
@@ -97,7 +104,8 @@ Handle<Value> Image::Generic_convert(void* pData) {
   switch (data->action) {
   case eNew3: 
   case eWriteFile:
-    aResult = ((Image*) data->retVal.pointer)->handle_; break;
+    aResult = ((Image*) data->retVal.pointer)->handle_;
+    break;
   case eWrite1:
   case eWrite2:
   case eWrite3: {
